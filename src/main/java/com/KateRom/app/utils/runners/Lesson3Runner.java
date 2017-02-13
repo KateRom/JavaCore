2b946b66214028374cd490d05511f368d17c4615
KateRomaniv <kate.romaniv@gmail.com>
2017-02-13 16:57:25 +0200

    Lesson3Runner modification

--- src/main/java/com/KateRom/app/utils/runners/Lesson3Runner.java
+++ src/main/java/com/KateRom/app/utils/runners/Lesson3Runner.java
@@ -3,10 +3,6 @@ package com.KateRom.app.utils.runners;
 import com.KateRom.app.utils.convertors.PrimitiveConvertor;
 
 public class Lesson3Runner {
-    float aFloat = 7.2f;
-    int anInt = 8;
-    char aChar = 'C';
-
         public static void main (String args[]) {
             PrimitiveConvertor convert1 = new PrimitiveConvertor();
             convert1.floatToChar();
@@ -14,7 +10,7 @@ public class Lesson3Runner {
             PrimitiveConvertor convert2 = new PrimitiveConvertor();
             convert2.intToChar();
 
-        PrimitiveConvertor convert3  = new PrimitiveConvertor();
-        convert3.charToInt();
+            PrimitiveConvertor convert3  = new PrimitiveConvertor();
+            convert3.charToInt();
         }
 }
