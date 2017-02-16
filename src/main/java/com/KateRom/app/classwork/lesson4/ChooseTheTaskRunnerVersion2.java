6617ce2ec910699a387e1d02e9d8aa6648ae9581
KateRomaniv <kate.romaniv@gmail.com>
2017-02-16 18:54:42 +0200

    Lesson4 homework ChooseTheTaskRunner rework, handle exception added

--- src/main/java/com/KateRom/app/classwork/lesson4/ChooseTheTaskRunnerVersion2.java
+++ src/main/java/com/KateRom/app/classwork/lesson4/ChooseTheTaskRunnerVersion2.java
@@ -11,17 +11,22 @@ public class ChooseTheTaskRunnerVersion2 {
                 "\n" + "3 - to identify bigger circle area." +
                 "\n" + "4 - to identify odd number." +
                 "\n" + "5 - to identify bigger number.");
-        int callValue = scanner.nextInt();
-        if (callValue == 1) {
-            CalculateTriangleRunner.menu1();
-        } else if (callValue == 2) {
-            CircleAreaRunner.menu2();
-        } else if (callValue == 3) {
-            CalculateBiggerAreaRunner.menu3();
-        } else if (callValue == 4) {
-            OddNumberRunner.menu4();
-        } else if (callValue == 5) {
-            BiggerNumberRunner.menu5();
-        } else System.out.println("You enter the number out fo range. Please enter right valuer (1, 2, 3, 4 or 5)");
+            try {
+                int callValue = scanner.nextInt();
+                if (callValue == 1) {
+                    CalculateTriangleRunner.menu1();
+                } else if (callValue == 2) {
+                    CircleAreaRunner.menu2();
+                } else if (callValue == 3) {
+                    CalculateBiggerAreaRunner.menu3();
+                } else if (callValue == 4) {
+                    OddNumberRunner.menu4();
+                } else if (callValue == 5) {
+                    BiggerNumberRunner.menu5();
+                } else
+                    System.out.println("You enter the number out fo range. Please enter right valuer (1, 2, 3, 4 or 5)");
+            }catch (java.util.InputMismatchException e) {
+            System.out.println("You entered inappropriate value. Please enter one of next values 1, 2, 3, 4 or 5");
+        }
     }
 }
