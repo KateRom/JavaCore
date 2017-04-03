e4f385a8657522df1926971b5fc0737bfd5e67bd
KateRomaniv <kate.romaniv@gmail.com>
2017-04-03 18:58:08 +0300

    Lesson 6 homework/classwork additional

--- src/main/java/com/KateRom/runners/homework/lesson6/DuplexArrayMatrix.java
+++ src/main/java/com/KateRom/runners/homework/lesson6/DuplexArrayMatrix.java
@@ -1,24 +1,21 @@
-/*package com.KateRom.runners.homework.lesson6;
+package com.KateRom.runners.homework.lesson6;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class DuplexArrayMatrix {
     public static void main(String[] args) {
-        final int[][] duplexArray = new int[8][5];
-        printMatrix(duplexArray);
-    }
-    public static void printMatrix(int[][] duplexArray) {
+        int[][] duplexArray = new int[8][5];
         Random randomGenerator = new Random();
-        Arrays.stream(duplexArray);
-
         for (int i = 0; i < duplexArray.length; i++) {
             for (int j = 0; j < duplexArray[i].length; j++) {
                 int a = randomGenerator.nextInt(999 + 1 - 1) + 1;
                 duplexArray[i][j] = a;
-                System.out.print(duplexArray[i][j] + " ");
-                System.out.println();
             }
         }
+        for (int row = 0; row<8; row++){
+            for (int col=0; col<5; col++){
+                System.out.print(duplexArray[row][col]+" ");
+            } System.out.print("\n");
+        }
     }
-}*/
+}
